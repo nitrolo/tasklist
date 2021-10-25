5a75768be31496c3199b3d438ffc33fd77619bc6
“nitrolo” <nitrolo225@gmail.com>
2021-10-26 03:19:54 +0530

    added TextInput and Button

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View></View>
+    <View style={{ padding: 100 }}>
+      <View>
+        <TextInput
+          placeholder="Add Goal"
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="+" />
+      </View>
       <View></View>
     </View>
   );
